06b284128d592df4558bfe1995aa681e547d40b0
njugunagrace <njugunagraceewambui@gmail.com>
2024-11-02 02:35:44 +0300

    Made adjustments to settings.py

--- sentiment_analysis/settings.py
+++ sentiment_analysis/settings.py
@@ -41,10 +41,11 @@ INSTALLED_APPS = [
     'reviews',
 ]
 
-STATIC_URL = '/reviews/static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'reviews/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
@@ -127,7 +128,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
